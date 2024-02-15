faa7a1b8c8b43758099503c1322fad579aba990e
ManuMan32 <141773224+ManuMan32@users.noreply.github.com>
2024-02-15 17:29:37 -0300

    Improve main title and background

--- src/components/maincontent/maintitle/MainTitle.tsx
+++ src/components/maincontent/maintitle/MainTitle.tsx
@@ -1,16 +1,32 @@
 import "./MainTitle.css";
+import { useEffect, useRef } from "react";
 interface Props {
   title: string
 }
 const MainTitle: React.FC<Props> = ({ title }) => {
   const titleArray = title.split("");
+  titleArray.unshift("<");
+  titleArray.push(">");
+  const effectIntervalRef = useRef<number>(0);
+  useEffect(() => {
+    effectIntervalRef.current = setInterval(() => {
+      const letters: NodeListOf<HTMLElement> = document.querySelectorAll(".mainTitleLetters_Letter");
+      letters.forEach((letter, i) => {
+        setTimeout(() => letter.style.animation = `1s ease forwards letterAnimation`, 100 * i);
+        setTimeout(() => letter.style.animation = `none`, 1000 + (100 * i));
+      })
+    }, 3000);
+    return (() => clearInterval(effectIntervalRef.current));
+  })
   return (
     <div className="mainTitle">
       <div className="mainTitleLetters">
         {titleArray.map((letter, i) => {
-          return <span className="mainTitleLetters_Letter" key={i}>
-            {(letter == " ") ? "-" : letter}
-            </span>
+          return <span
+            className={(letter == " ") ? "mainTitleLetters_Space" : "mainTitleLetters_Letter"}
+            key={i}>
+            {letter}
+          </span>
         })}
       </div>
     </div>
