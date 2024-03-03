375eb23ee4ba9ebb4fa5d3fce52ca6f71a2e6004
ManuMan32 <141773224+ManuMan32@users.noreply.github.com>
2024-03-03 11:20:59 -0300

    Responsive media query max width 1150px

--- src/components/maincontent/aside/Aside.tsx
+++ src/components/maincontent/aside/Aside.tsx
@@ -7,22 +7,26 @@ const Aside: React.FC = () => {
   const { articles, isLoading, recomendations } = useGlobalContext();
   return (
     <aside className="aside">
-      <Subtitle>Other articles:</Subtitle>
-      {(isLoading) ? (<span>Loading...</span>)
-        : (<>
-          <AsideArticlePreview articleObj={articles[recomendations[0]]} />
-          <AsideArticlePreview articleObj={articles[recomendations[1]]} />
-          <AsideArticlePreview articleObj={articles[recomendations[2]]} />
-        </>)}
-      <p style={{
-        textAlign: 'center',
-        textIndent: '0',
-        opacity: '0.5'
-      }}>I'm a 17 y/o boy sharing content about code and self improvement. Studying to be a future programmer and fulfill my dreams.</p>
-      <Subtitle>Social Media:</Subtitle>
-      <AsideSocialMedia name="Twitter" image="/iconTwitter.svg" link="https://twitter.com/ManuCro32" />
-      <AsideSocialMedia name="YouTube" image="/iconYoutube.svg" link="https://www.youtube.com/channel/UCq6abTyDgfHaiMEutdbmdqw" />
-      <AsideSocialMedia name="GitHub" image="/iconGithub.svg" link="https://github.com/ManuMan32" />
+      <div className="asideContent1">
+        <Subtitle>Other articles:</Subtitle>
+        {(isLoading) ? (<span>Loading...</span>)
+          : (<>
+            <AsideArticlePreview articleObj={articles[recomendations[0]]} />
+            <AsideArticlePreview articleObj={articles[recomendations[1]]} />
+            <AsideArticlePreview articleObj={articles[recomendations[2]]} />
+          </>)}
+      </div>
+      <div className="asideContent2">
+        <p style={{
+          textAlign: 'center',
+          textIndent: '0',
+          opacity: '0.5'
+        }}>I'm a 17 y/o boy sharing content about code and self improvement. Studying to be a future programmer and fulfill my dreams.</p>
+        <Subtitle>Social Media:</Subtitle>
+        <AsideSocialMedia name="Twitter" image="/iconTwitter.svg" link="https://twitter.com/ManuCro32" />
+        <AsideSocialMedia name="YouTube" image="/iconYoutube.svg" link="https://www.youtube.com/channel/UCq6abTyDgfHaiMEutdbmdqw" />
+        <AsideSocialMedia name="GitHub" image="/iconGithub.svg" link="https://github.com/ManuMan32" />
+      </div>
     </aside>
   )
 }
