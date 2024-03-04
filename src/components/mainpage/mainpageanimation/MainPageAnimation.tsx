bef9280a19bc25e7f9fea7c6caf10ba575698882
ManuMan32 <141773224+ManuMan32@users.noreply.github.com>
2024-03-04 20:53:04 -0300

    Remove main animation when animations are off

--- src/components/mainpage/mainpageanimation/MainPageAnimation.tsx
+++ src/components/mainpage/mainpageanimation/MainPageAnimation.tsx
@@ -1,21 +1,27 @@
+import { useGlobalContext } from "../../../globalContext";
 import "./MainPageAnimation.css";
 import { useEffect, useRef, useState } from "react";
 let iterations = 1;
 const limitIterations = 7;
 const MainPageAnimation: React.FC = () => {
+  const { animations } = useGlobalContext();
   const [image, setImage] = useState<string>("1.png");
   const effectIntervalRef = useRef<number>(0);
   useEffect(() => {
     effectIntervalRef.current = setInterval(() => {
       const movingDivs = document.getElementsByClassName("mainPageAnimationWindowBox")[0].childNodes;
       const fixType = movingDivs as NodeListOf<HTMLElement>
-      fixType[0].style.animation = '3s ease-in-out infinite moveDiv';
-      fixType[1].style.animation = '3s ease-in-out .1s infinite moveDiv';
-      fixType[2].style.animation = '3s ease-in-out .2s infinite moveDiv';
+      if (fixType[0] != undefined) {
+        fixType[0].style.animation = '3s ease-in-out infinite moveDiv';
+        fixType[1].style.animation = '3s ease-in-out .1s infinite moveDiv';
+        fixType[2].style.animation = '3s ease-in-out .2s infinite moveDiv';
+      }
       setTimeout(() => {
-        fixType[0].style.animation = 'none';
-        fixType[1].style.animation = 'none';
-        fixType[2].style.animation = 'none';
+        if (fixType[0] != undefined) {
+          fixType[0].style.animation = 'none';
+          fixType[1].style.animation = 'none';
+          fixType[2].style.animation = 'none';
+        }
         iterations++;
         if (iterations > limitIterations) iterations = 1;
         setImage(`${iterations}.png`);
@@ -27,9 +33,13 @@ const MainPageAnimation: React.FC = () => {
     <div className="mainPageAnimation">
       <div className="mainPageAnimationBox">
         <div className="mainPageAnimationWindowBox">
-          <div className="mainPageAnimationWindow"></div>
-          <div className="mainPageAnimationWindow"></div>
-          <div className="mainPageAnimationWindow"></div>
+          {(animations) ? (
+            <>
+              <div className="mainPageAnimationWindow"></div>
+              <div className="mainPageAnimationWindow"></div>
+              <div className="mainPageAnimationWindow"></div>
+            </>
+          ) : void 0}
         </div>
         <img className="mainPageAnimationImg" src={image} />
       </div>
